bd08192cce572843a2d9f0e106f0a963acea5b1f
juanstangaferro23-dot <juanstangaferro23@gmail.com>
2026-07-01 21:45:41 -0300

    committ

---
+++ Ejemplos/seleccionesconbanderas.js
@@ -0,0 +1,75 @@
+// Archivo físico: selecciones con banderas.js
+const datosSelecciones = {    
+    "Grupo A": [
+        {"posicion": 1, "pais": "México", "bandera_url": "https://flagcdn.com/w160/mx.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 2, "pais": "Sudáfrica", "bandera_url": "https://flagcdn.com/w160/za.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 3, "pais": "Corea del Sur", "bandera_url": "https://flagcdn.com/w160/kr.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 4, "pais": "República Checa", "bandera_url": "https://flagcdn.com/w160/cz.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0}
+    ],
+    "Grupo B": [
+        {"posicion": 1, "pais": "Canadá", "bandera_url": "https://flagcdn.com/w160/ca.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 2, "pais": "Bosnia y Herzegovina", "bandera_url": "https://flagcdn.com/w160/ba.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 3, "pais": "Catar", "bandera_url": "https://flagcdn.com/w160/qa.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 4, "pais": "Suiza", "bandera_url": "https://flagcdn.com/w160/ch.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0}
+    ],
+    "Grupo C": [
+        {"posicion": 1, "pais": "Brasil", "bandera_url": "https://flagcdn.com/w160/br.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 2, "pais": "Marruecos", "bandera_url": "https://flagcdn.com/w160/ma.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 3, "pais": "Haití", "bandera_url": "https://flagcdn.com/w160/ht.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 4, "pais": "Escocia", "bandera_url": "https://flagcdn.com/w160/gb-sct.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0}
+    ],
+    "Grupo D": [
+        {"posicion": 1, "pais": "Estados Unidos", "bandera_url": "https://flagcdn.com/w160/us.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 2, "pais": "Paraguay", "bandera_url": "https://flagcdn.com/w160/py.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 3, "pais": "Australia", "bandera_url": "https://flagcdn.com/w160/au.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 4, "pais": "Turquía", "bandera_url": "https://flagcdn.com/w160/tr.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0}
+    ],
+    "Grupo E": [
+        {"posicion": 1, "pais": "Alemania", "bandera_url": "https://flagcdn.com/w160/de.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 2, "pais": "Curaçao", "bandera_url": "https://flagcdn.com/w160/cw.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 3, "pais": "Costa de Marfil", "bandera_url": "https://flagcdn.com/w160/ci.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 4, "pais": "Ecuador", "bandera_url": "https://flagcdn.com/w160/ec.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0}
+    ],
+    "Grupo F": [
+        {"posicion": 1, "pais": "Países Bajos", "bandera_url": "https://flagcdn.com/w160/nl.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 2, "pais": "Japón", "bandera_url": "https://flagcdn.com/w160/jp.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 3, "pais": "Suecia", "bandera_url": "https://flagcdn.com/w160/se.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 4, "pais": "Túnez", "bandera_url": "https://flagcdn.com/w160/tn.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0}
+    ],
+    "Grupo G": [
+        {"posicion": 1, "pais": "Bélgica", "bandera_url": "https://flagcdn.com/w160/be.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 2, "pais": "Egipto", "bandera_url": "https://flagcdn.com/w160/eg.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 3, "pais": "Irán", "bandera_url": "https://flagcdn.com/w160/ir.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 4, "pais": "Nueva Zelanda", "bandera_url": "https://flagcdn.com/w160/nz.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0}
+    ],
+    "Grupo H": [
+        {"posicion": 1, "pais": "España", "bandera_url": "https://flagcdn.com/w160/es.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 2, "pais": "Cabo Verde", "bandera_url": "https://flagcdn.com/w160/cv.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 3, "pais": "Arabia Saudita", "bandera_url": "https://flagcdn.com/w160/sa.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 4, "pais": "Uruguay", "bandera_url": "https://flagcdn.com/w160/uy.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0}
+    ],
+    "Grupo I": [
+        {"posicion": 1, "pais": "Francia", "bandera_url": "https://flagcdn.com/w160/fr.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 2, "pais": "Senegal", "bandera_url": "https://flagcdn.com/w160/sn.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 3, "pais": "Irak", "bandera_url": "https://flagcdn.com/w160/iq.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 4, "pais": "Noruega", "bandera_url": "https://flagcdn.com/w160/no.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0}
+    ],
+    "Grupo J": [
+        {"posicion": 1, "pais": "Argentina", "bandera_url": "https://flagcdn.com/w160/ar.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 2, "pais": "Argelia", "bandera_url": "https://flagcdn.com/w160/dz.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 3, "pais": "Austria", "bandera_url": "https://flagcdn.com/w160/at.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 4, "pais": "Jordania", "bandera_url": "https://flagcdn.com/w160/jo.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0}
+    ],
+    "Grupo K": [
+        {"posicion": 1, "pais": "Portugal", "bandera_url": "https://flagcdn.com/w160/pt.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 2, "pais": "RD Congo", "bandera_url": "https://flagcdn.com/w160/cd.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 3, "pais": "Uzbekistán", "bandera_url": "https://flagcdn.com/w160/uz.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 4, "pais": "Colombia", "bandera_url": "https://flagcdn.com/w160/co.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0}
+    ],
+    "Grupo L": [
+        {"posicion": 1, "pais": "Inglaterra", "bandera_url": "https://flagcdn.com/w160/gb-eng.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 2, "pais": "Croacia", "bandera_url": "https://flagcdn.com/w160/hr.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 3, "pais": "Ghana", "bandera_url": "https://flagcdn.com/w160/gh.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0},
+        {"posicion": 4, "pais": "Panamá", "bandera_url": "https://flagcdn.com/w160/pa.png", "PJ": 0, "G": 0, "E": 0, "P": 0, "GF": 0, "GC": 0, "DG": 0, "Pts": 0}
+    ]
+}
